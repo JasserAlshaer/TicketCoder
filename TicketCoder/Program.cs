371ac02b8f8292756034a5b6013a3c6e7dc95151
ASUS <ASUS@DESKTOP-SV986PK>
2024-01-24 19:11:20 +0300

    EditSwagger

--- TicketCoder/Program.cs
+++ TicketCoder/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using TicketCoder.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Ticket Coder API",
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddDbContext<TicketCoderDbContext>(c =>
 c.UseSqlServer(builder.Configuration.GetValue<string>("sqlconnect")));
 var app = builder.Build();
